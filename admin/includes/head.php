bb46ae2e8a2df56acfed73b162be779057c12d01
tharanamayu <tharana.maya@gmail.com>
2019-01-04 22:02:55 +0530

    adding heading  file

---
+++ admin/includes/head.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html >
+<head>
+
+    <title>Administrator</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/main.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0,user-scalable=no">
+    
+    <script src="../ajax/jquery.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container-fluid">
